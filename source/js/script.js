c32bcba4d07202a00cb6177b98b30bc71a67cfd8
Liubov Kartsivadze <kartsivadzelubov@mail.ru>
2021-05-04 15:04:58 +0300

    Внесла исправления

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,15 @@
 var navMain = document.querySelector('.main-nav');
   var navToggle = document.querySelector('.main-nav__toggle');
 
-  navMain.classList.remove('main-nav_nojs');
+  navMain.classList.remove('main-nav--nojs');
 
   navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains('main-nav_closed')) {
-      navMain.classList.remove('main-nav_closed');
-      navMain.classList.add('main-nav_opened');
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
     } else {
-      navMain.classList.add('main-nav_closed');
-      navMain.classList.remove('main-nav_opened');
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
     }
   });
 
